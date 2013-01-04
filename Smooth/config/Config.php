e2f77c92079f85b7b05fdc7845a6ed191c9135af
smoothframework <support@smoothphp.com>
2013-01-04 11:12:00 +0200

    Add the config file

---
+++ Smooth/config/Config.php
@@ -0,0 +1,14 @@
+<?php
+	
+	/*
+	* Current Smooth version 0.0.1 does not support the method of changing the configuration using the Config.php file;
+	* You can config your app through the index.php file
+	* Please read the documentation.
+	 */
+
+	namespace Smooth\Config;
+
+	class Config
+	{
+
+	}
