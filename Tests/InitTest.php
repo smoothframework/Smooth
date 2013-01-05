3fe81364d41b559c725beba601f0ba148fedda3e
smoothframework <support@smoothphp.com>
2013-01-05 10:38:29 +0200

    Add the Init phpunit test

---
+++ Tests/InitTest.php
@@ -0,0 +1,19 @@
+<?php
+
+	include '../index.php';
+
+	class InitTest extends PHPUnit_Framework_TestCase
+	{
+		
+		public function test_smooth_vars()
+		{		
+			$this->assertTrue(defined('BASEPATH'));
+			$this->assertTrue(defined('APPPATH'));
+			$this->assertTrue(defined('SYSPATH'));
+			$this->assertTrue(defined('WEBPATH'));
+			$this->assertTrue(defined('CONTROLLER'));
+			$this->assertTrue(defined('SMOOTH_V'));
+			$this->assertTrue(isset($_SERVER['REQUEST_URI']));
+		}
+
+	}
