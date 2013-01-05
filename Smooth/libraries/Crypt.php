fefa532be245dc96e93846e0a9c17542e042e4d4
smoothframework <support@smoothphp.com>
2013-01-06 00:21:53 +0200

    Update

--- Smooth/libraries/Crypt.php
+++ Smooth/libraries/Crypt.php
@@ -2,6 +2,8 @@
 
 	namespace Smooth\Libraries;
 
+	
+
 	/**
 	 * @package    Smooth Crypt Library
 	 */
@@ -9,13 +11,15 @@
 	class Crypt
 	{
 
-		public static function now($method, $string)
+		public static function crypter($method, $string)
 		{
-			$methods = array('sha1', 'sha224', 'sha256', 'sha384', 'sha512', 'md2', 'md4', 'md5', 'ripemd128', 'ripemd160', 'ripemd256',
+			$crypt_methods = array('sha1', 'sha224', 'sha256', 'sha384', 'sha512', 'md2', 'md4', 'md5', 'ripemd128', 'ripemd160', 'ripemd256',
 				'ripemd320', 'whirlpool', 'snefru', 'snefru256', 'gost', 'crc32', 'crc32b', 'adler32');
 
 			if( in_array( $method, $crypt_methods ) )
 				return hash($method, $string);
+			else
+				
 		}
 
 	}
