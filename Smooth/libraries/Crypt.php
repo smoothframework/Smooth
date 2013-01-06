21cf05d00b1153945cfbeed2c45f90c0b7051807
smoothframework <support@smoothphp.com>
2013-01-06 13:00:02 +0200

    Update the crypt library

--- Smooth/libraries/Crypt.php
+++ Smooth/libraries/Crypt.php
@@ -2,15 +2,18 @@
 
 	namespace Smooth\Libraries;
 
-	
-
 	/**
 	 * @package    Smooth Crypt Library
 	 */
 
 	class Crypt
 	{
-
+		/**
+		 * Crypt your strings with one of the hash algorithms
+		 * @param  string $method 
+		 * @param  string $string 
+		 * @return string         
+		 */
 		public static function crypter($method, $string)
 		{
 			$crypt_methods = array('sha1', 'sha224', 'sha256', 'sha384', 'sha512', 'md2', 'md4', 'md5', 'ripemd128', 'ripemd160', 'ripemd256',
@@ -19,7 +22,7 @@
 			if( in_array( $method, $crypt_methods ) )
 				return hash($method, $string);
 			else
-				
+				exit('Error occured during the crypter class');
 		}
 
 	}
