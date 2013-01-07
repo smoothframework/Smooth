691bb761050af761f3c249a3de4e2f740d4708bc
smoothframework <support@smoothphp.com>
2013-01-07 12:11:46 +0200

    Update Tests/InitTest.php

--- Tests/InitTest.php
+++ Tests/InitTest.php
@@ -10,10 +10,7 @@
 			$this->assertTrue(defined('BASEPATH'));
 			$this->assertTrue(defined('APPPATH'));
 			$this->assertTrue(defined('SYSPATH'));
-			$this->assertTrue(defined('WEBPATH'));
-			$this->assertTrue(defined('BASECONTROLLER'));
-			$this->assertTrue(defined('SMOOTH_V'));
-			$this->assertTrue(isset($_SERVER['REQUEST_URI']));
+
 		}
 
 	}
