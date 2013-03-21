05dd3989ab62b3b312976231c352f8d2be7a329a
Konstantin Rachev <support@smoothphp.com>
2013-03-21 22:36:48 +0200

    Add a testsuite files

---
+++ Tests/LibrariesTest.php
@@ -0,0 +1,34 @@
+<?php
+	
+	use Smooth\Libraries\Crypt;
+	use Smooth\Libraries\Generator;
+
+	class LibrariesTest extends PHPUnit_Framework_TestCase
+	{
+
+		public function setUp()
+		{
+			$this->string = "Smooth";
+		}
+
+		public function testIfCrypterLibraryReturnsAnAccurateResult()
+		{
+			$cryptMethods = ['sha1', 'sha224', 'sha256', 'sha384', 'sha512', 'md2', 'md4', 'md5', 'ripemd128', 'ripemd160', 'ripemd256',
+				'ripemd320', 'whirlpool', 'snefru', 'snefru256', 'gost', 'crc32', 'crc32b', 'adler32'];
+			foreach ($cryptMethods as $key => $value) 
+			{
+				$this->assertEquals(hash($value, $this->string), Crypt::crypter($value, $this->string));
+			}
+		}
+
+		public function testIfGeneratorLibraryReturnsAnAccurateResult()
+		{
+			$length = 5;
+			$generatorTypes = ['string', 'int', 'string_int'];
+			foreach ($generatorTypes as $key => $value) 
+			{
+				$this->assertStringMatchesFormat('%a', Generator::$value($length));
+			}
+		}
+
+	}
