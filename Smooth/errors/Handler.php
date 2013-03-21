8002dc713f9054afc88403ecec062fb1d6681fcd
smoothframework <support@smoothphp.com>
2013-03-21 22:50:09 +0200

    Update Handler.php

--- Smooth/errors/Handler.php
+++ Smooth/errors/Handler.php
@@ -2,28 +2,25 @@
 
 	namespace Smooth\Errors;
 
+	use Smooth\Controller;
+
 	class Handler
 	{
 
-		public function __construct()
+		public static function handler($errorMessage, $errorCode = null, $errorFile = null, $errorLine = null)
 		{
-			set_error_handler(array($this, 'errorHandler'));
-			set_exception_handler(array($this, 'exceptionHandler'));
-		}
+			$data = array(
 
-		public function errorHandler()
-		{
-			
-		}
+					'errorMessage' => $errorMessage,
+					'errorCode' => $errorCode,
+					'errorFile' => $errorFile,
+					'errorLine' => $errorLine,
+					'content' => 'ErrorView'
 
-		public function exceptionHandler()
-		{
-			
-		}
+				);
 
-		public static function handler()
-		{
-
+			Controller::render('includes/template', compact('data'));
+			exit();
 		}
 
 	}
